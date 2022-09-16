80a9c31f8119cb6a18f66aeb346409a55c11fc76
CristianoGambirasio <c.gambirasio6@studenti.unibg.it>
2022-09-16 15:46:42 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AppMenu from '../components/AppMenu.vue'
-import AppWorkspace from '../components/AppWorkspace.vue'
 
 Vue.use(VueRouter)
-
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/AppMenu',
-    name: 'menu',
-    component: AppMenu
-  },
-  {
-    path: '/AppWorkspace',
-    name: 'workspace',
-    component: AppWorkspace
   }
 ]
 
